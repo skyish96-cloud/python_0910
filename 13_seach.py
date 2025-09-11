6237edd60f10981c07a4de273c9bcb91a813bccb
기영 <rldud@naver.com>
2025-09-11 15:48:26 +0900

    13_seach

--- 13_seach.py
+++ 13_seach.py
@@ -1,4 +1,6 @@
 # 검색 -> 특정한 자료 덩어리에서 원하는 값을 찾는것
+from operator import truediv
+
 a = [3,4,1,2,3,4,'G','F','G']
 # 원하는 값에 인덱스 찾기
 # 2 라는 값은 어느 위치에 있는가?
@@ -42,3 +44,57 @@ for n in b: # for in을 이용하면 list에 있는 값을 순서대로 하나�
 #     idx = b.index(3,idx)
 #     print(f'3의 값은 {idx}번에 있다.')
 #     idx += 1
+
+# 리스트 요소 삭제
+# del a[3] 과 a.remove(3)
+# del은 특정 인덱스의 값을 지운다
+# remove는 해당 값을 지운다.(한개만)
+# - remove는 한개만 지우기때문에 여러번 해야한다.
+
+print(f'a : {a}')
+a.remove(3)
+print(f'a : {a}')
+
+# pop() = append()의 반대개념
+# append()는 뒤에서부터 순차적으로 숫자가 붙는 개념이였다면
+# pop()은 뒤에서부터 순차적으로 숫자가 빠지는 개념이다.(리스트에서 사라진다.)
+
+val = a.pop()
+print(f'빼낸 값 : {val} / a : {a}')
+val = a.pop()
+print(f'빼낸 값 : {val} / a : {a}')
+
+#리스트 확장(더하기와 비슷한 개념)
+print(a)
+a.extend(b)
+print(a)
+
+# count(val) 특정한 값이 해당 리스트에 몇개 있는지 확인
+print(a)
+print(f'a 안에 3은 {a.count(4)}개 가 있다.')
+print(f'a 안에 9는 {a.count(9)}개 가 있다.') # 없는 값은 0을 반환한다.
+
+# a안에 있는 모든 3은 지워주세요.
+a = [3,4,1,2,3,4,'G','F','G']
+# print(f'a : {a}')
+# a.remove(3)
+# print(f'a : {a}')
+
+
+# 가장 많이 하는 방법
+# a = [3,4,1,2,3,4,'G','F','G']
+# for n in a:
+#    if n == 3:
+#      a.remove(3)
+
+while True: # 조건이 무조건 참이 될 경우 계속해서 실행 되어 자동으로 멈출 수 없다.
+    a.remove(3) # 3을 지우게 하는 것
+    if a.count(3) == 0: # 3이 남은게 있는지 확인
+        break # 3이 없으면 멈추도록 함
+print(a)
+
+
+
+
+
+
