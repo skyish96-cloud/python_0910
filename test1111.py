943cac4015f0ec38fdfaa732ab288fffca1b5573
skyish96-cloud <skyish96@gmail.com>
2025-09-12 00:00:06 +0900

    Create test1111.py

---
+++ test1111.py
@@ -0,0 +1,16 @@
+id = 'hong'
+password = ('1234')
+id_chk=input('아이디 입력 :').strip()
+if id_chk == id:
+    pwd_chk=input('비밀번호 입력 :')
+    if pwd_chk == password:
+        print(f'{id_chk} 님 로그인에 성공했습니다.')
+    else:
+        print(f'{pwd_chk} 비밀번호가 맞지 않습니다.')
+else:
+    print(f'{id_chk} 등록되지 않은 아이디 입니다.')
+
+
+
+
+
