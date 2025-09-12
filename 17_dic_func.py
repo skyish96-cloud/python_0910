3aca99a755b4a9f9593513e7dc01736e3718f65f
기영 <rldud@naver.com>
2025-09-12 11:39:31 +0900

    dic 함수소개

--- 17_dic_func.py
+++ 17_dic_func.py
@@ -7,6 +7,9 @@ dic = {
 
 # dic.keys() : 특정한 사전의 키들만 가져와 dict_keys라는 객체를 반환한다.
 print(dic.keys())
-
 for item in dic.keys():
     print(item)
+
+# dict_keys -> list로 변환
+keys = list(dic.keys())
+print(keys)
