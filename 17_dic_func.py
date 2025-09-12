8305aaf399f2b86cce1ae7ae711d02c421903701
기영 <rldud@naver.com>
2025-09-12 11:34:32 +0900

    dic 함수소개

--- 17_dic_func.py
+++ 17_dic_func.py
@@ -3,3 +3,10 @@ dic = {
     'phone':'010-1234-1234',
     'friends':['Alice','Smith','Jhone']
 }
+
+
+# dic.keys() : 특정한 사전의 키들만 가져와 dict_keys라는 객체를 반환한다.
+print(dic.keys())
+
+for item in dic.keys():
+    print(item)
