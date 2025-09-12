141882485650b341566c9ed2d5e724cf32e8d876
기영 <rldud@naver.com>
2025-09-12 13:15:01 +0900

    set소개

--- 18_set.py
+++ 18_set.py
@@ -6,3 +6,26 @@ print(number_set)
 # 그래서 중복 제거 용도로 사용된다.
 str_set = set("Hello World")
 print(str_set)
+
+#set들을 이용해서 집합을 구현할 수 있다.
+s1 = set([1,2,3,4,5,6])
+s2 = set([4,5,6,7,8,9])
+
+# 교집합(intersection), (&가 아닌 intersection을 입력해도 교집합이 가능하다.)
+print(f'교집합1 : {s1 & s2}')
+print(f'교집합2 : {s1.intersection(s2)}')
+
+# 합집합(union), (|가 아닌 union을 입력해도 교집합이 가능하다.)
+print(f'합집합1 : {s1 | s2}')
+print(f'합집합2 : {s1.union(s2)}')
+
+#
+
+
+
+
+
+
+
+
+
