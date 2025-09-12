234ff7da0c8b04e83d6a3729883d9dc18258c411
기영 <rldud@naver.com>
2025-09-12 14:33:41 +0900

    함수 기본

---
+++ 19_function.py
@@ -0,0 +1,15 @@
+# 파이썬에서는 def를쓰면 된다.
+# def은 선언이다.
+#JAVA와 다르다.
+
+# 함수 선언(define)
+def toaster(bread):
+    print(f'{bread}이 구워지고 있다.')
+    return f'구워진 {bread}'
+    pass
+
+# 함수 사용
+dish = toaster('식빵')
+print(dish)
+#dish의 역활은 상태를 확인하기 위해서이다.
+
