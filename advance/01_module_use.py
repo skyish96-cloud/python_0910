fab81c8c3f41be6035ba6e8f324e173d57056eda
기영 <rldud@naver.com>
2025-09-15 11:46:26 +0900

    모듈사용 3

--- advance/01_module_use.py
+++ advance/01_module_use.py
@@ -3,16 +3,14 @@
 from oper import sum
 print(f'sum() 함수실행 : {sum(5,10)}')
 
-
-
 # import 모듈
 # 일단 모듈로 불러놓고 모듈로부터 원하는 함수를 사용하는 방법
 
-# 1.
+# 1. oper이라는 이름의 모듈을 불러옴
 import oper
 print(f'minus() 함수실행 : {oper.minus(10,5)}')
 
-# 2. 별칭을 준다.
+# 2. oper에 op라는 별칭을 준다.
 import oper as op
 print(f'minus() 함수실행 : {op.minus(10,5)}')
 
