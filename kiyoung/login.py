2d6c0269c87346c3cb9e34d3005bd84a3b5bd3dd
기영 <rldud@naver.com>
2025-09-15 17:32:42 +0900

    로그인 기본

--- kiyoung/login.py
+++ kiyoung/login.py
@@ -1,15 +1,14 @@
 id = 'hong'
-password = ('1234')
+password = '1234'
 id_chk=input('아이디 입력 :').strip()
 if id_chk == id:
     pwd_chk=input('비밀번호 입력 :')
     if pwd_chk == password:
         print(f'{id_chk} 님 로그인에 성공했습니다.')
     else:
-        print(f'{pwd_chk} 비밀번호가 맞지 않습니다.')
+        print(' 비밀번호가 맞지 않습니다.')
 else:
-    print(f'{id_chk} 등록되지 않은 아이디 입니다.')
-
+    print(' 등록되지 않은 아이디 입니다.')
 
 
 
