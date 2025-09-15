7b47775b5685eeb39a55559ad372115db40a9009
기영 <rldud@naver.com>
2025-09-15 17:27:14 +0900

    모듈사용4

--- advance/02_module_list.py
+++ advance/02_module_list.py
@@ -8,3 +8,20 @@ print(oper.__name__) # 특정 모듈의 이름을 확인
 print(__name__) # 현재 나의 이름 -> 현재 실행되는 함수
 # (자바에서는 메인 메서드를 실행하기 위해서는 메인 메서드를 사용해야한다.
 # 하지만 파이썬에서는 메인 메서드를 사용하지 않아도 메인 메서드를 실행한다.)
+
+# class_active(활동과 관련한 함수)
+# run()
+# jump()
+# walk()
+# swim()
+
+# class_수학적 함수
+# plus()
+# minus()
+# multiple()
+# devide()
+
+# class_drive(운전과 관련한 함수)
+# acceleration()
+# break()
+# drift()
