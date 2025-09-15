6c622ab9728aa8b5aa226b553edc9f7196cf9417
기영 <rldud@naver.com>
2025-09-15 11:03:42 +0900

    함수 API 설명

--- 22_api.py
+++ 22_api.py
@@ -1,7 +1,17 @@
 
 def print_max(x,y):
+    '''
+    입력된 x와 y 중 큰 값을 알려주는 함수입니다.
+    :param x: 인자값 1
+    :param y: 인자값 2
+    '''
     print(f'{x}와 {y}중에...');
     if x > y:
         print(f'{x}가 더 큽니다.')
     else:
         print(f'{y}가 더 큽니다.')
+
+print_max(5,10)
+
+# 해당 함수에 대한 출력
+print(f'함수 설명:{print_max.__doc__}')
