7cdcd3cd00d94efaba0f2cc2ef00e433979ba1f4
기영 <rldud@naver.com>
2025-09-15 11:40:15 +0900

    모듈사용 2

--- advance/01_module_use.py
+++ advance/01_module_use.py
@@ -5,5 +5,10 @@ print(f'sum() 함수실행 : {sum(5,10)}')
 
 
 # import 모듈
+# 1.
 import oper
 print(f'minus() 함수실행 : {oper.minus(10,5)}')
+
+# 2.
+import oper as op
+print(f'minus() 함수실행 : {op.minus(10,5)}')
