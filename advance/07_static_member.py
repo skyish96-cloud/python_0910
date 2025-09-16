d9e3f7f2174b58f9c499efb1fff12bec7b26beb4
오준영 <ohjunyoung@email.com>
2025-09-16 10:55:34 +0900

    정적멤버2

--- advance/07_static_member.py
+++ advance/07_static_member.py
@@ -6,3 +6,13 @@ class Robot:
 
     def std_count(self):
         print(f'클래스 메서드 :{self.count}')
+
+r1 = Robot()
+r2 = Robot()
+# r1 과 r2는 서로 다른 객체이므로 count를 건들였을때 서로 영향받지 않는다.
+r1.count +=1
+print(f'r1.count : {r1.count}')
+print(f'r2.count : {r2.count}')
+r2.count = 100
+print(f'r1.count : {r1.count}')
+print(f'r2.count : {r2.count}')
